be3b265222ae0b18843ab23a84e11437d1c028a5
Buferxon <buferxon@gmail.com>
2025-03-10 16:34:14 -0500

    correciones

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->integer('identification_number')->unique()->nullable()->after('name');
+            $table->integer('identification_number')->unique()->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
